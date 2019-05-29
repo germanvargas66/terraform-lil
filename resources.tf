2571a81b9ae643762b3cbd67d346d0faa9ea9faf
German Vargas <gvargas@C02T13SPH03Q.local>
2019-05-28 20:40:27 -0400

    updated resources.tf

--- resources.tf
+++ resources.tf
@@ -3,11 +3,10 @@ resource "google_compute_network " "our_development_network" {
   auto_create_subnetworks = true
 }
 
-resource "aws_vpcm" "environment-example-two" {
+resource "aws_vpc" "environment-example-two" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
-
   tags = {
     Name = "terraform-aws-vpc-example-two"
   }
