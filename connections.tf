634832be9fee9f9582fba7c6ee03bd29f7a1e42b
German Vargas <gvargas@c02t13sph03q.mustard.ipcoop.com>
2019-07-19 11:39:39 -0400

    "added files"

--- connections.tf
+++ connections.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("../account.json")}"
+  credentials = file("../account.json")
   project     = "global-cursor-241512"
   region      = "us-west1"
 }
@@ -8,18 +8,25 @@ provider "aws" {
   region = "us-west-2"
 }
 
-provider "vsphere" {
-}
+
+
 
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
+variable "subscription_id" {
+}
 
-variable subscription_id {}
-variable client_id {}
-variable client_secret {}
-variable tenant_id {}
+variable "client_id" {
+}
+
+variable "client_secret" {
+}
+
+variable "tenant_id" {
+}
+
